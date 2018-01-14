4bf5e4e8cfd5d5e5169f3b4d12bedf4393c9dd47
Pram Attale <pram@attale.com>
2018-01-14 11:23:18 +0000

    adding initial

---
+++ src/main/java/com/naughtyzombie/eth/springeth/SpringEthApplication.java
@@ -0,0 +1,12 @@
+package com.naughtyzombie.eth.springeth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEthApplication.class, args);
+	}
+}
